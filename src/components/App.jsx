0ed5e4acfaf08d9be55da65e517aef36090e00fa
Bohdan Sova <sova.bogdan.94@gmail.com>
2023-05-24 17:51:20 +0200

    debug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import SearhBar from './searchBar/SearchBar';
+import SearhBar from './SearchBar/SearchBar';
 import ImageGallery from './image-gallery/ImageGallery';
 import LoadMoreButton from './button/Button';
 import { AppContainer } from './App.styled';
